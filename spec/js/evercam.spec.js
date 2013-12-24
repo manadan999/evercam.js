4d29b9e4ea587866787d25d56b46c0d205bf3caf
GvS666 <gvs666@gmail.com>
2013-12-24 15:44:25 +0000

    Model

--- spec/js/evercam.spec.js
+++ spec/js/evercam.spec.js
@@ -1,5 +1,9 @@
 describe("Evercam", function() {
 
+  $( document ).ajaxError(function( event, request, settings ) {
+    console.log( "Error requesting page " + settings.url );
+  });
+
   var subject = Evercam;
 
   describe("#apiUrl", function() {
@@ -9,8 +13,34 @@ describe("Evercam", function() {
     });
 
     it("can be overridden to another url", function() {
-      subject.setApiUrl('http://localhost');
-      expect(subject.apiUrl).toBe('http://localhost');
+      subject.setApiUrl('http://localhost:3000/v1');
+      expect(subject.apiUrl).toBe('http://localhost:3000/v1');
+    });
+
+  });
+
+  describe("Models", function() {
+
+    it("get all", function(done) {
+      subject.Model.all(function(vendors) {
+        expect(vendors.length).toBe(2);
+        done();
+      });
+    });
+
+    it("get by vendor", function(done) {
+      subject.Model.by_vendor('testid', function(vendor) {
+        expect(vendor.id).toBe('testid');
+        done();
+      });
+    });
+
+    it("get by model", function(done) {
+      subject.Model.by_model('testid', 'YCW005', function(model) {
+        expect(model.vendor).toBe('testid');
+        expect(model.name).toBe('YCW005');
+        done();
+      });
     });
 
   });
--- src/js/evercam.js
+++ src/js/evercam.js
@@ -1,4 +1,4 @@
-(function(window) {
+(function(window, $) {
 
   "use strict";
 
@@ -8,8 +8,34 @@
 
     setApiUrl: function(url) {
       this.apiUrl = url;
+    },
+
+    Model: {
+      url: function(ext){
+        if (typeof(ext) === 'undefined') ext = '';
+        else ext = '/' + ext;
+        return window.Evercam.apiUrl + '/models' + ext;
+      },
+
+      all: function(callback) {
+        $.getJSON(this.url(), function(data) {
+          callback(data.vendors);
+        });
+      },
+
+      by_vendor: function (vid, callback) {
+        $.getJSON(this.url(vid), function(data) {
+          callback(data.vendors[0]);
+        });
+      },
+
+      by_model: function (vid, mid, callback) {
+        $.getJSON(this.url(vid + '/' + mid), function(data) {
+          callback(data.models[0]);
+        });
+      }
     }
 
   };
 
-})(window);
+})(window, jQuery);
